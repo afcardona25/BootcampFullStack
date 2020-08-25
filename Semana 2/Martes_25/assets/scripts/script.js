15b061125a6c401c2a16814caa105a4ba88b2be4
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-08-25 14:43:21 -0500

    nuevos documentos

--- Semana 2/Martes_25/assets/scripts/script.js
+++ Semana 2/Martes_25/assets/scripts/script.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var boton = document.getElementById("btn")
-
-})
+var boton = document.getElementById("btn")
+console.log("este es el botón", boton)
+/* El código se detiene al encontrar un alert. Es necesario que el usuario cierre el alert cuando aparezca para que el código se siga leyendo*/
+alert("Bienvenidos")
+var nombre = prompt("Escribe tu nombre", "Pedro Pérez")
+alert("Bienvenido " + nombre)
