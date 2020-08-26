5271a4f1f63d18b6b17c972bb9fd4f1ad9c9c529
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-08-26 15:18:54 -0500

    inicio de clase miercoles 26

---
+++ Semana 2/Miercoles_26/Clase/script.js
@@ -0,0 +1,29 @@
+//tipos de datos primarios 
+
+//undefined y null
+var a = undefined
+var b = null
+
+//booleanos
+var c = true
+var d = false
+
+//numbers
+var e = 9
+
+//string 
+var f = "string"
+
+//symbol 
+var g = Symbol('foo')
+
+//objetos 
+
+var primerArray = [1,2,3,4]
+var arrayDic = {
+    FirstNAme : "Chris",
+    LastNAme : "Doe",
+    Age : 23
+}
+console.log(typeof arrayDic)
+//Es mejor usar let que var!
