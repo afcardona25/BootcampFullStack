dba549a38ec9c2a5bb51aed729f151732e160128
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-08-27 17:30:38 -0500

    correcciones en lógica y apriencia

--- Semana 2/Miercoles_26/Actividad/script.js
+++ Semana 2/Miercoles_26/Actividad/script.js
@@ -1,30 +1,49 @@
 
+
 let resultado = 0
 function operaciones(x) {
     let num1 = Number(document.getElementById("numero1").value)
     let num2 = Number(document.getElementById("numero2").value)
     console.log(typeof num1)
     console.log(typeof num2)
-    switch(x){
-        case suma: 
-            resultado = num1 + num2
-            alert("El resultad de la suma es:  "+resultado )
-            document.getElementById("logSumas").innerHTML += num1 +  "+"+ num2 +"="+resultado+ "<br></br>"
-            break
-        case resta: 
-            resultado = num1 - num2
-            alert("El resultado de la resta es:  " + resultado)
-            document.getElementById("logRestas").innerHTML += num1 + "-" + num2 + "=" + resultado + "<br></br>"
-            break
-        case multiplicacion:
-            resultado = num1 * num2
-            alert("El resultado de la multiplicación es:  " + resultado)
-            document.getElementById("logMultiplicaciones").innerHTML += num1 + "*" + num2 + "=" + resultado + "<br></br>"
-            break
-        case division:
-            resultado = num1 / num2
-            alert("El resultado de la división es:  " + resultado)
-            document.getElementById("logDivisiones").innerHTML += num1 + "/" + num2 + "=" + resultado + "<br></br>"
-            break
+    if (num1 != 0 && num2 != 0) {
+        switch (x) {
+            case suma:
+                resultado = num1 + num2
+                swal("Suma", "El resultad de la suma es " + resultado, "success", {
+                    button: "Sigue calculando",
+                })
+
+                document.getElementById("logSumas").innerHTML += num1 + "+" + num2 + "=" + resultado + "<br></br>"
+                break
+            case resta:
+                resultado = num1 - num2
+                swal("Resta", "El resultad de la suma es " + resultado, "success", {
+                    button: "Sigue calculando",
+                })
+                document.getElementById("logRestas").innerHTML += num1 + "-" + num2 + "=" + resultado + "<br></br>"
+                break
+            case multiplicacion:
+                resultado = num1 * num2
+                swal("Multiplicación", "El resultad de la suma es " + resultado, "success", {
+                    button: "Sigue calculando",
+                })
+                document.getElementById("logMultiplicaciones").innerHTML += num1 + "*" + num2 + "=" + resultado + "<br></br>"
+                break
+            case division:
+                resultado = num1 / num2
+                swal("División", "El resultad de la suma es " + resultado, "success", {
+                    button: "Sigue calculando",
+                })
+                document.getElementById("logDivisiones").innerHTML += num1 + "/" + num2 + "=" + resultado + "<br></br>"
+                break
+        }
+        document.getElementById("numero1").value = ""
+        document.getElementById("numero2").value = ""
+    } else {
+        swal("ERROR!", "Por favor ingresa 2 valores para calcular ", "error", {
+            button: "Intentarlo Otra vez!",
+        })
     }
+   
 }
