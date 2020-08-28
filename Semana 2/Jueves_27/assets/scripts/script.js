0ffe32820dad986a4a7b5ac6efc87e4cca60a52e
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-08-27 19:23:09 -0500

    creacion de carpetas 27 agosto y primer commit actividad

---
+++ Semana 2/Jueves_27/assets/scripts/script.js
@@ -0,0 +1,9 @@
+let errorsign = "*"
+function validarFormulario(){
+    let nombre = document.getElementById("nombre").value
+    let apellido = document.getElementById("apellido").value
+    let correo = document.getElementById("correo").value
+    let genero = document.getElementById("genero").value
+    let cedula = document.getElementById("cedula").value
+    let tyc = document.getElementById("aceptarTyC").value
+}
