3e26d42b5c352fb28dd12417993d5977552e2358
Andres Felipe Cardona <62449597+afcardona25@users.noreply.github.com>
2020-08-29 12:13:53 -0500

    finalizacion actividad

--- Semana 2/Jueves_27/assets/scripts/script.js
+++ Semana 2/Jueves_27/assets/scripts/script.js
@@ -1,13 +1,17 @@
 let errorsign = "*"
+
+
+
+
 function validarFormulario() {
     let nombre = document.getElementById("nombre").value
     let apellido = document.getElementById("apellido").value
     let correo = document.getElementById("correo").value
     let genero = document.getElementById("genero").value
     let cedula = document.getElementById("cedula").value
-    
+
     console.log(nombre + " "+ apellido+" "+correo + " "+ genero+" "+cedula+" ")
-    if (nombre != "" && apellido != "" && correo != "" && genero != "" && cedula != "" ){
+    if (nombre != "" && apellido != "" && correo != "" && genero != "defaul" && cedula != "" ){
         if (formulario.aceptarTyC.checked == true){
             swal({
                 title: "Bienvenido ",
@@ -15,6 +19,7 @@ function validarFormulario() {
                 icon: "success",
                 button: "Sigue a la Calculadora",
             });
+            window.location.href = "../Miercoles_26/Actividad/index.html"
         } else {
             document.getElementById("TyC").classList.add("error")
             document.getElementById("TyC").innerText += errorsign
@@ -29,39 +34,20 @@ function validarFormulario() {
 
         return false
     } else {
-        while (nombre == ""){
-            document.getElementById("nombre").classList.add("error")
-            document.getElementById("nombre").innerText += errorsign
-        }
-        while (apellido == ""){
-            document.getElementById("apellido").classList.add("error")
-            document.getElementById("apellido").innerText += errorsign
-        }
-        while (correo == "") {
-            document.getElementById("correo").classList.add("error")
-            document.getElementById("correo").innerText += errorsign
-        }
-        while (genero == "") {
-            document.getElementById("genero").classList.add("error")
-            document.getElementById("genero").innerText += errorsign
-        }
-        while (cedula == "") {
-            document.getElementById("cedula").classList.add("error")
-            document.getElementById("cedula").innerText += errorsign
-        }
-        while (formulario.aceptarTyC.checked == false) {
-            document.getElementById("TyC").classList.add("error")
-            document.getElementById("TyC").innerText += errorsign
-        }
+  
+
         swal({
             title: "Error!",
             text: "Debes llenar todos los datos",
             icon: "error",
             button: "Corregir Datos",
         });
-        return false
+      
+        
+        return false 
     }
     return true
     
     
 }
+
